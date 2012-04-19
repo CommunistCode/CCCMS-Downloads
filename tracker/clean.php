69c777ebe59b13fa5f9ab1e8649925e92d92a977
Jack Mitchell <jack@communistcode.co.uk>
2012-04-19 13:32:51 +0100

    remove tracker, too much hassel, just use opentracker instead

--- tracker/clean.php
+++
@@ -1,7 +0,0 @@
-<?
-require_once 'config.inc.php';
-
-mysql_connect($db_server, $db_user, $db_pass);
-mysql_select_db($db_db);
-mysql_query("DELETE FROM `$db_table` WHERE `expire_time` < NOW() - INTERVAL 3 DAY");
-?>
